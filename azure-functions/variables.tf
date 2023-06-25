d166e42acce05b19e1e2f057417c75e2f4433f0c
Tosiek2134 <105588618+Tosiek2134@users.noreply.github.com>
2023-06-25 12:02:07 +0200

    Create variables.tf

---
+++ azure-functions/variables.tf
@@ -0,0 +1,39 @@
+variable "resource-group-name" {
+  type = string
+}
+
+variable "resource-group-location" {
+  type = string
+}
+
+variable "storage_account-name" {
+  type = string
+}
+
+variable "storage_account-account_tier" {
+  type = string
+}
+
+variable "storage_account-replication_type" {
+  type = string
+}
+
+variable "app_service_plan-name" {
+  type = string
+}
+
+variable "app_service_plan-sku-tier" {
+  type = string
+}
+
+variable "app_service_plan-sku-size" {
+  type = string
+}
+
+variable "linux_function_app-name" {
+  type = string
+}
+
+variable "function_app_function-name" {
+  type = string
+}
